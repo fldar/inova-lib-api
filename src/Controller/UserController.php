7003c9580361dd5ebbbb2e7c297e19528f3b5b72
Francisco Lucas <lucasalmeidacco@gmail.com>
2021-01-05 16:29:45 -0300

    S02T14 - Create Route delete user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\UserService;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\{JsonResponse, Request};
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -18,7 +19,7 @@ class UserController extends ApiAbstractController
     private const
         CREATED_SUCCESS = 'The user %s was successfully created',
         UPDATED_SUCCESS = 'The user %s was successfully updated',
-        DELETED_SUCCESS = 'The user was successfully deleted'
+        DELETED_SUCCESS = 'The user %s was successfully deleted'
     ;
 
     private UserService $userService;
@@ -50,22 +51,23 @@ class UserController extends ApiAbstractController
         $request = $this->getRequestContent($request);
         $user = $this->userService->createUser($request);
 
-        return $this->json([
-            "message" => sprintf(self::CREATED_SUCCESS, $user->getName())
-        ]);
+        return $this->json(["message" => sprintf(self::CREATED_SUCCESS, $user->getUsername())]);
     }
 
     /**
      * @IsGranted("ROLE_USER_ADMIN")
      * @Route("/delete/id/{id}", name="user_delete", methods={"GET"})
+     * @param Request $request
+     * @param int $id
      * @return JsonResponse
+     * @throws NonUniqueResultException
      */
     public function deleteUser(Request $request, int $id): JsonResponse
     {
         $request = $this->getRequestContent($request);
-        $this->userService->deleteUser($id, $request);
+        $user = $this->userService->deleteUser($id, $request);
 
-        return $this->json(["message" => self::DELETED_SUCCESS]);
+        return $this->json(["message" => sprintf(self::DELETED_SUCCESS, $user->getUsername())]);
     }
 
     /**
@@ -73,13 +75,13 @@ class UserController extends ApiAbstractController
      * @Route("/set-roles", name="user_set_roles", methods={"POST"})
      * @param Request $request
      * @return JsonResponse
-     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws NonUniqueResultException
      */
     public function setRoles(Request $request): JsonResponse
     {
         $request = $this->getRequestContent($request);
         $user = $this->userService->setUserRoles($request);
 
-        return $this->json(["message" => sprintf(self::UPDATED_SUCCESS, $user->getName())]);
+        return $this->json(["message" => sprintf(self::UPDATED_SUCCESS, $user->getUsername())]);
     }
 }
